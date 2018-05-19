a1620a5044e22cb90d926015c177278fca850d10
Takahiro Suzuki <t.suzuki@techd.jp>
2018-05-19 23:16:41 +0900

    draft version

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
+    }
+    lat: number = 35.777764;
+    lng: number = 139.720912;
+
+    facebook() {
+        window.open('https://www.facebook.com/takahiro.suzuki.9216', '_blank')
+    }
+
+    twitter() {
+        window.open('https://twitter.com/_suzutt', '_blank')
+    }
 
 }
